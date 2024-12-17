7b4deaf5bda70a9eeef050b27b4a2b4583df81b1
LamTruongVan <tvlama23144@cusc.ctu.edu.vn>
2024-12-17 16:41:04 +0700

    first commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://tvlama23144:Lam05082005@cluster0.dqxug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/users');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
